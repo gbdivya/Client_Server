9a23352d1ccd8e900ed21b1f7986e05f9ca6d494
gbdivya <73168441+gbdivya@users.noreply.github.com>
2021-06-15 13:53:53 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,16 +3,26 @@ import socket
 ServerSocket = socket.socket()
 
 print("Socket Created")
-host= '192.168.246.45'
-port= 3000
-
-ServerSocket.bind((host,port))
+HOST= socket.gethostname()   
+PORT= 5000
+ServerSocket.bind((HOST,PORT))
 ServerSocket.listen(5)
 
 print('Waitiing for a Connection..')
+print("\n")
 
 while True:
     ClientSocket, addr = ServerSocket.accept()
     name = ClientSocket.recv(1024).decode()
-    print( name, ": Connected" )
+    print('Connected by : ', name, addr )
+    message = ClientSocket.recv(1024).decode()
+    print('Message is : ', message)
+    #filename=input(str("Enter the file name: "))
+    filename=ClientSocket.recv(1024).decode()
+    file=open(filename,'rb')
+    file_data=file.read(1024)
+    ClientSocket.send(file_data)
+    print("Done sending")
+    print("\n")
+    
     ClientSocket.close()
