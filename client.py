fa394dfa93e5785feee3a092e286f99e52d75239
gbdivya <73168441+gbdivya@users.noreply.github.com>
2021-06-15 13:53:07 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,11 +1,26 @@
 import socket
 
 ClientSocket = socket.socket()
-host= '192.168.246.45'
-port= 3000
-ClientSocket.connect((host,port))
+HOST= socket.gethostname() 
+PORT= 5000
+ClientSocket.connect((HOST,PORT))
+print("Connected Successfully")
 
-name = input('Write Something: ')
+name = input('Write your name: ')
 ClientSocket.send(bytes(name, 'utf-8'))
+
+
+message = input('Write your message: ')
+ClientSocket.send(bytes(message, 'utf-8'))
+
+filename=input(str("Enter the file name: "))
+ClientSocket.send(bytes(filename, 'utf-8'))
+
+file=open(filename,'wb')
+file_data=ClientSocket.recv(1024)
+file.write(file_data)
+file.close()
+print("File received")
+print('connection closed')
 
 print(ClientSocket.recv(1024).decode())
